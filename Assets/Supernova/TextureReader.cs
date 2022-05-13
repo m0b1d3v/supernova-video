646ffc27b119e4b65084398b9e3788c62b80c9e3
Mobius K <mail@mobiusk.com>
2022-05-13 00:44:41 -0400

    Building out a few example scenes for camera, USharpVideo, and ProTV

--- Assets/Supernova/TextureReader.cs
+++ Assets/Supernova/TextureReader.cs
@@ -1,50 +1,84 @@
-﻿using UdonSharp;
+﻿using System;
+using UdonSharp;
 using UnityEngine;
-using VRC.Udon;
 
 namespace Supernova
 {
+    /**
+     * Toggles children components based on given pixel intensities from a TextureSampler script 
+     */
     public class TextureReader : UdonSharpBehaviour
     {
 
         /**
-         * TextureSampler script runner that is always processing render texture changes
-         */
-        [SerializeField, Tooltip("Video render image script output")]
-        private UdonBehaviour textureSampler;
-        
-        /**
-         * Array index for pixels we can grab from the textureSampler above
+         * Array index for pixels that are given to us from a TextureSampler script
          */
         [SerializeField, Tooltip("Which pixel to read from the video render image script output for processing")]
-        private int samplePixelIndex;
+        private int samplePixelIndex = 5;
 
-        [SerializeField, Tooltip("Child objects underneath this given parent will be toggled based on parent intensity")]
-        private GameObject parentOfChildrenToToggle;
-        
+        /**
+         * Variance bracket range for intensity activating an object
+         */
+        [SerializeField, Tooltip("How much pixel intensity range will allocate to activating each child, from 1 to 100")]
+        private int intensityRangeForEachChild;
+
         /**
          * Flag for if we are currently processing a pixel update from the event loop and should skip further processing
          */
         private bool _processing;
 
-        void Update()
+        /**
+         * Called by TextureSampler scripts to determine if it should call ToggleChildren() 
+         */
+        public int GetSamplePixelIndex()
         {
+            return samplePixelIndex;
+        }
 
-            // Bail out early if we are working on a past event or there is no texture to process
-            if (_processing || textureSampler == null || parentOfChildrenToToggle == null)
+        /**
+         * Called from TextureSampler script on every render frame
+         */
+        public void ToggleChildren(float intensity)
+        {
+            // Bail out early if we are working on a past event
+            if (_processing)
             {
                 return;
             }
-            
+
             // Set flag to show that we are working an event in case another occurs before we are done
             _processing = true;
             
-            // Sample the given pixel intensity and deactivate all children not within 5% of the intensity
+            // Determine which child object (if any) should be activated and others deactivated based on intensity
 
-            Color[] textureData = (Color[]) textureSampler.GetProgramVariable("textureData");
-            if(textureData.Length > samplePixelIndex)
+            var activatedChild = -1;
+
+            if (intensityRangeForEachChild > 0 && intensityRangeForEachChild <= 100)
             {
-                Debug.Log(textureData[samplePixelIndex].grayscale);
+                intensity = Math.Max(0, intensity);
+                intensity = Math.Min(1, intensity);
+                var percentage = (int) (intensity * 100);
+                activatedChild = percentage / intensityRangeForEachChild;
+            }
+
+            // Sample the given pixel intensity and deactivate all children not within given intensity range
+
+            var children = GetComponentsInChildren<Transform>(true);
+            for (var childIndex = 0; childIndex < children.Length; childIndex++)
+            {
+
+                // Skip this if we're running on the parent (Unity's "getChildren" is a liar) or no change is necessary 
+                
+                var child = children[childIndex].gameObject;
+                var desiredChildState = childIndex == activatedChild;
+                
+                if (child.activeSelf == desiredChildState || child.GetInstanceID() == gameObject.GetInstanceID())
+                {
+                    continue;
+                }
+                
+                // Toggle activation state of objects
+                child.gameObject.SetActive(desiredChildState);
             }
 
             // Set flag to show that we are done working this event and can process newer events
--- Assets/Supernova/TextureSampler.cs
+++ Assets/Supernova/TextureSampler.cs
@@ -3,6 +3,9 @@ using UnityEngine;
 
 namespace Supernova
 {
+    /**
+     * Samples a portion of a video render texture to a texture image every so often and hands to TextureReader scripts
+     */
     public class TextureSampler : UdonSharpBehaviour
     {
         /**
@@ -30,24 +33,31 @@ namespace Supernova
         private int pixelColumnSampleHeight;
 
         /**
-         * Desired pixel colors are saved here after mostly-every render frame for other scripts to pick up and use 
+         * Event processors listed here instead of running an Update() loop on every texture reader
          */
-        public Color[] textureData;
+        [SerializeField, Tooltip("Texture readers that will run their own processing whenever a new sample is taken")]
+        private TextureReader[] textureReaders;
 
         /**
-         * Flag for if we are currently grabbing pixels from an OnPostRender event and should skip further processing
+         * Flags for if we are currently grabbing pixels from an OnPostRender event and should skip further processing
          */
+        private int _lastFrameCalculation;
         private bool _processing;
 
+        /**
+         * Event that fires on component creation that determines whether or not we ever handle render loop
+         */
         void Start()
         {
             // Make sure that we aren't given bad input on the pixel sampling
             if (texture2D == null
+                || textureReaders == null
+                || textureReaders.Length == 0
                 || texture2D.width < xPixelColumnSamplePosition
                 || texture2D.height < yPixelColumnSamplePosition + pixelColumnSampleHeight
             )
             {
-                Debug.LogWarning("Pixel sample impossible with given inputs.");
+                Debug.LogWarning("TextureSampler cannot work with bad user input, shutting down");
                 texture2D = null;
             }
         }
@@ -58,18 +68,19 @@ namespace Supernova
         void OnPostRender()
         {
             
-            // Bail out early if we are working on a past event or there is no texture to process
-            if (_processing || texture2D == null)
+            // Bail out early if we are working on a past event, there is no texture to process, or we should wait a bit
+            if (_processing || texture2D == null || Time.frameCount - _lastFrameCalculation < 10)
             {
                 return;
             }
-            
+
             // Set flag to show that we are working an event in case another occurs before we are done
             _processing = true;
+            _lastFrameCalculation = Time.frameCount;
 
             // Read a pixel column from the render texture into our image texture
             
-            Rect sampleSpace = new Rect(
+            var sampleSpace = new Rect(
                 xPixelColumnSamplePosition,
                 yPixelColumnSamplePosition,
                 1,
@@ -77,10 +88,36 @@ namespace Supernova
             );
             
             texture2D.ReadPixels(sampleSpace, 0, 0, false);
-            textureData = texture2D.GetPixels(0, 0, 1, pixelColumnSampleHeight);
+            var textureData = texture2D.GetPixels(0, 0, 1, pixelColumnSampleHeight);
+            
+            Debug.LogWarning("Texture sampler: " + textureData[0]);
+            
+            // Alert all of our texture readers that there is a new pixel intensity to process
+
+            foreach (var textureReader in textureReaders)
+            {
+
+                // Skip any bad inputs in the script list
+                if (textureReader == null)
+                {
+                    continue;
+                }
+
+                // Skip any input requesting a pixel that we don't have
+                var samplePixelIndex = textureReader.GetSamplePixelIndex();
+                if (samplePixelIndex < 0 || samplePixelIndex >= textureData.Length)
+                {
+                    continue;
+                }
+
+                // Send the TextureReader an intensity value for it to process
+                var intensity = textureData[samplePixelIndex].grayscale;
+                textureReader.ToggleChildren(intensity);
+            }
 
             // Set flag to show that we are done working this event and can process newer events
             _processing = false;
         }
+
     }
 }
